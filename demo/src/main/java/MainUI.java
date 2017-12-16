e844d4437b722bafc81eaffaf5007ea8e743e64b
Nurmukhanov <olzhas.nurmukhanov@gmail.com>
2017-12-16 19:59:55 +0600

    multi module settings

--- demo/src/main/java/MainUI.java
+++ demo/src/main/java/MainUI.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import brains.vframework.VUI;
+import com.vaadin.server.VaadinRequest;
 
-public class MainUI {
+
+public class MainUI extends VUI{
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+    }
 }
