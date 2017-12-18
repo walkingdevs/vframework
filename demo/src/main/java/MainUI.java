2339e5841e3d10f88589a961caef69ee10ee5318
Nurmukhanov <olzhas.nurmukhanov@gmail.com>
2017-12-18 17:49:56 +0600

    whats wrong

--- demo/src/main/java/MainUI.java
+++ demo/src/main/java/MainUI.java
@@ -1,10 +1,19 @@
 import brains.vframework.VUI;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
 
-
-public class MainUI extends VUI{
+@Title("Main UI")
+@Theme("base")
+public class MainUI extends VUI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        setContent(layout);
 
+        Label label = new Label("Hello world");
+        layout.addComponent(label);
     }
 }
