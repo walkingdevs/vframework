413d4f783b0ad7fdc4152e5f460df2f09844103e
Nurmukhanov <olzhas.nurmukhanov@gmail.com>
2017-12-19 06:49:05 +0600

    fixed

--- demo/src/main/java/MainUI.java
+++ demo/src/main/java/MainUI.java
@@ -2,18 +2,17 @@ import brains.vframework.VUI;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.VerticalLayout;
 
 @Title("Main UI")
-@Theme("base")
+@Theme("blue")
 public class MainUI extends VUI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
         setContent(layout);
-
-        Label label = new Label("Hello world");
-        layout.addComponent(label);
+        Button button = new Button("OK");
+        layout.addComponent(button);
     }
 }
---
+++ framework/webapp/VAADIN/widgetsets/brains.vframework.AppWidgetSet/deferredjs/64ACCBFAD75DB5BDE1CCBC1F9CCD00F5/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.brains_vframework_AppWidgetSet.runAsyncCallback2('zhb(1682,1,xae);_.vd=function dlc(){t3b((!m3b&&(m3b=new y3b),m3b),this.a.d)};X3d(Tk)(2);\n//# sourceURL=brains.vframework.AppWidgetSet-2.js\n')
