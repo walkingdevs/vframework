5f822c1ce7e4bdf20a73a2984ed730d91c201e97
Nurmukhanov <olzhas.nurmukhanov@gmail.com>
2017-12-30 23:24:54 +0600

    injection

--- demo/src/main/java/view/MainUI.java
+++ demo/src/main/java/view/MainUI.java
@@ -1,12 +1,11 @@
 package view;
 
 import brains.vframework.VUI;
+import brains.vframework.view.ListView;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.guice.annotation.GuiceUI;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.VerticalLayout;
 
 
 @Title("Main UI")
@@ -16,16 +15,6 @@ public class MainUI extends VUI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        Button button = new Button("Create", (event) -> {
-        });
-        Button button2 = new Button("Get", (event) -> {
-        });
-
-        VerticalLayout layout = new VerticalLayout(
-            button,
-            button2
-        );
-        layout.setSizeFull();
-        setContent(layout);
+        setContent(new ListView());
     }
 }
