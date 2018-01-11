9a296842086c35c27595021c18b9fc8958bb59c0
Nurmukhanov <olzhas.nurmukhanov@gmail.com>
2018-01-11 14:34:41 +0600

    vaadin

--- demo/src/main/java/brains/Main.java
+++ demo/src/main/java/brains/Main.java
@@ -1,6 +1,7 @@
 package brains;
 
 import brains.entity.Person;
+import brains.vframework.embedded.VaadinBundle;
 import brains.vframework.embedded.Vframework;
 
 public class Main {
@@ -10,6 +11,7 @@ public class Main {
             .mk()
             .basePackages("brains.dao", "brains.rs")
             .entities(Person.class)
+            .vaadinBundle(new VaadinBundle(ApplicationServlet.class, "/foo/*"))
             .build()
             .run(args);
     }
